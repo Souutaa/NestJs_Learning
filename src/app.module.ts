bf0be2721e79959680695e862be42599229c470e
Shikigami Maito <51812674+maitogami123@users.noreply.github.com>
2023-10-13 06:29:38 +0000

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "posgres",
+      password: "posgres",
+      database: "task-management",
+      autoLoadEntities: true,         // in nestjs you will have to create entities, và đó là cách mà tụi entities được chuyển qua thành tables/schema thông qua sự trợ giúp của typeorm
+      // có thể dùng autoLoadEntities và để cho typeorm tự tìm các file entity và load chúng một cách tự động hoặc 
+      // bạn cũng có thể tự chọn ra các entity mà bạn muốn load
+      synchronize: true,
+      // giúp cho các bảng trong database với schema luôn đồng bộ với nhau. Hoặc cũng có thể sử dụng migration
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
