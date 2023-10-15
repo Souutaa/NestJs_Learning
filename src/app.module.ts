bdfb2ad6e0fe4d000f17af2d9108ca72d433b0e4
Quang Long <trinhq011@gmail.com>
2023-10-15 20:51:49 +0700

    update use npm

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TasksModule,
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
+      type: 'postgres',
+      host: 'localhost',
       port: 5432,
-      username: "posgres",
-      password: "posgres",
-      database: "task-management",
-      autoLoadEntities: true,         // in nestjs you will have to create entities, và đó là cách mà tụi entities được chuyển qua thành tables/schema thông qua sự trợ giúp của typeorm
-      // có thể dùng autoLoadEntities và để cho typeorm tự tìm các file entity và load chúng một cách tự động hoặc 
+      username: 'posgres',
+      password: 'posgres',
+      database: 'task-management',
+      autoLoadEntities: true, // in nestjs you will have to create entities, và đó là cách mà tụi entities được chuyển qua thành tables/schema thông qua sự trợ giúp của typeorm
+      // có thể dùng autoLoadEntities và để cho typeorm tự tìm các file entity và load chúng một cách tự động hoặc
       // bạn cũng có thể tự chọn ra các entity mà bạn muốn load
       synchronize: true,
       // giúp cho các bảng trong database với schema luôn đồng bộ với nhau. Hoặc cũng có thể sử dụng migration
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
